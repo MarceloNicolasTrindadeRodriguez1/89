a204bda6e6c6d561c613d2d66c84fa55b69e3e31
MarceloNicolasTrindadeRodriguez <82178471+MarceloNicolasTrindadeRodriguez@users.noreply.github.com>
2023-10-03 15:10:10 -0400

    avancement

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+
+function calculeSalaire(){
+    const TauxHoraire = parseFloat(document.getElementById('TauxHoraire').value);
+    const heureSemaine = parseFloat(document.getElementById('heureSemaine').value);
+
+    console.log(TauxHoraire);
+    console.log(heureSemaine);
+    
+
+     
+}
+
+
+
+
+
+
